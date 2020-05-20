5def3fcd431aaa7ad9fe711a0ad4c6a647a1315e
Lauren Mitchell <laurenmit98@bitbucket.org>
2020-05-20 09:45:38 +0200

    initial commit
    Setup

---
+++ cable_sizing_calc/cable_sizing_calc/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cable_sizing_calc project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cable_sizing_calc.settings')
+
+application = get_wsgi_application()
